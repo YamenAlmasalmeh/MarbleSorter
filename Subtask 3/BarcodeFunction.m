a26e0afa647bff5ade29674a25d5df2b66267862
YamenAlMasalmeh <YamenAlMasalmeh@gmail.com>
2018-04-02 20:34:51 -0500

    merged the two versions that were being used

--- Subtask 3/BarcodeFunction.m
+++ Subtask 3/BarcodeFunction.m
@@ -1,19 +1,18 @@
 function [ colors ] = BarcodeFunction(motorA, sensor )
-motorA.Speed = -20;
+motorA.Speed = -30;
+resetRotation(motorA);
 start(motorA);
 past = 0;
 colors = [];
 while abs(readRotation(motorA)) < 840
-    while abs(readRotation(motorA)) - past < 91
+    while abs(readRotation(motorA)) - past < 89
     end
     stop(motorA)
-    pause(1);
-    colors = [colors, readLightIntensity(sensor)];
+    colors = [colors, readLightIntensity(sensor, 'reflected')];
     past = abs(readRotation(motorA));
     start(motorA);
 end
-stop(motorA);
-colors = colors <= 2;
+colors = colors <= 50;
 
 end
 
