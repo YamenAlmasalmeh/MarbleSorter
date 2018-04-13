b8e7d45391097e795a40b55702e2a7e784d576fa
YamenAlMasalmeh <YamenAlMasalmeh@gmail.com>
2018-04-13 15:45:17 -0500

    Subtask 3 done

--- Subtask 3/BarcodeFunction.m
+++ Subtask 3/BarcodeFunction.m
@@ -12,6 +12,10 @@ while abs(readRotation(motorA)) < 840
     past = abs(readRotation(motorA));
     start(motorA);
 end
+stop(motorA);
+colors = colors(2:length(colors));
+colors(1:3) = fliplr(colors(1:3));
+colors(4:8) = fliplr(colors(4:8));
 colors = colors <= 50;
 
 end
